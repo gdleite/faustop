80166dbe7a36a7ae6d8fca0a9899c6a6bc44a268
gdleite <leite06@Hotmail.com>
2017-09-18 10:16:07 -0300

    Update faustao.js

--- faustao.js
+++ faustao.js
@@ -1,10 +1,10 @@
-var respotas={1:"",2:"",3:"",4:"",5:"",6:"",7:"",8:"",9:"",10:"",11:"",12:"",13:"",14:""}
-console.log(respotas)
-var t=document.getElementById('ss')
-console.log(t)
+var respostas={1:"",2:"",3:"",4:"",5:"",6:"",7:"",8:"",9:"",10:"",11:"",12:"",13:"",14:""}
+console.log(respostas)
 
-function resposta(pergunta, valor){
-	var x = valor
-	respostas.push({pergunta:x})
-	console.log(respostas)
+
+function test(pergunta,valor){
+	var x = valor;
+	var y = pergunta;
+	respostas[y]=x;
+	console.log(respostas);
 }
